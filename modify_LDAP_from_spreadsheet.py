d84194cd1310d5b7313b29cfee7f3ec41102eacc
Ghenya <evasyanina25@gmail.com>
2014-04-14 11:28:01 -0700

    Update modify_LDAP_from_spreadsheet.py

--- modify_LDAP_from_spreadsheet.py
+++ modify_LDAP_from_spreadsheet.py
@@ -1,3 +1,7 @@
+# This script modifies title, job type and manager of the employee in the LDAP directory
+# using data provided in Google spreadsheet. In this particular script email address of the employee
+# was used as filter for search and modification.
+
 import gspread
 import getpass
 import ldap
@@ -17,28 +21,28 @@ try:
         password_ldap = getpass.getpass()
         l.simple_bind_s(username, password_ldap)
 
-#loop through each entry in the spreadsheet (set the range) to get data
-	for i in range(1, 100):
+#loop through each entry in the spreadsheet to get data
+	for i in range(1, 100): #set the range for entries in the spreadsheet
 		email_string = []
-		email_string.append('D') //column in the sreadsheet with email addresses
+		email_string.append('D') #column in the sreadsheet with email addresses
 		email_string.append(str(i))
 		email_joined = ''.join(email_string)	
 		cell_email = google_doc.acell(email_joined).value
 	
 		title_string = []
-		title_string.append('E') //column in the spreadsheet with employee title
+		title_string.append('E') #column in the spreadsheet with employee title
 		title_string.append(str(i))
 		title_joined = ''.join(title_string)
 		cell_title = google_doc.acell(title_joined).value
 
 		type_string = []
-		type_string.append('H') //column in the spreadsheet with job type
+		type_string.append('H') #column in the spreadsheet with job type
 		type_string.append(str(i))
 		type_joined = ''.join(type_string)
 		cell_type = google_doc.acell(type_joined).value
 
 		manager_string = []
-                manager_string.append('G') //column in the spreadsheet with the employee manager
+                manager_string.append('G') #column in the spreadsheet with the employee manager
                 manager_string.append(str(i))
                 manager_joined = ''.join(manager_string)
                 cell_manager = google_doc.acell(manager_joined).value
